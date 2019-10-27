1a972fc485a7399d337942af7a7d9600b024292f
PawelMurawski <pawelt.murawski@gmail.com>
2019-10-28 00:39:25 +0100

    31.3

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -13,7 +13,7 @@ public class FacebookTestingApp {
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-        driver.get("https://facebook.com");
+        driver.get("https://www.facebook.com");
 
         while (!driver.findElement(By.xpath(XPATH_DAY)).isDisplayed());
         WebElement daySearch = driver.findElement(By.xpath(XPATH_DAY));
